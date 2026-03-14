b15b8e1716280357c0d3fd9ab6d3d0707f14ec41
Andreas Backström <mozzo242@gmail.com>
2026-03-14 21:18:51 +0100

    Add dependencies vite-markdown-plugin, tailwind-typography and supabase

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import tailwindcss from '@tailwindcss/vite'
+import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
+		mdPlugin({ mode: [Mode.HTML]}),
 		preact({
 			prerender: {
 				enabled: true,
